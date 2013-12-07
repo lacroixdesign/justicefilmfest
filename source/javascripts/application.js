d7da552d658838b15ed14b9aff5a5f75549342ec
Michael LaCroix <michael@lacroixdesign.net>
2013-12-07 14:56:49 -0600

    add google map

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,1 +1,4 @@
+//= require ondomready.min
+//= require map/style
+//= require map/init
 //= require_self
---
+++ source/javascripts/map/init.js
@@ -0,0 +1,34 @@
+/* globals google, onDomReady */
+
+(function() {
+
+  var coordinates = new google.maps.LatLng(34.050286, -118.24568699999998);
+
+  var opts = {
+    zoom: 16,
+    center: coordinates,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: window.MapStyles,
+    scrollwheel: false,
+    streetViewControl: false,
+    mapTypeControl: false,
+    panControl: false,
+    zoomControlOptions: {style: 'SMALL'}
+  };
+
+  onDomReady(function initMap () {
+    var map = new google.maps.Map(document.getElementById('map'), opts);
+
+    var marker = new google.maps.Marker({
+      position: coordinates,
+      animation: google.maps.Animation.DROP
+    });
+    marker.setMap(map);
+
+    var infoWindow = new google.maps.InfoWindow({
+      content: "<h3>Justice Film Festival</h3><strong>Downtown Independent</strong><br>251 S. Main Street<br>Los Angeles, CA 90012"
+    });
+    infoWindow.open(map, marker);
+  });
+
+})();
