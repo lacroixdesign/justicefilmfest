a8e37e188845a5dcc42e0391c757213ee8b9f55a
Michael LaCroix <michael@lacroixdesign.net>
2013-12-10 20:08:57 -0600

    add FastClick

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,5 +1,16 @@
 //= require modernizr.min
+//= require addeventlistener.shim
+//= require fastclick
 //= require ondomready.min
 //= require map/style
 //= require map/init
 //= require_self
+
+/* globals FastClick */
+
+/*
+ * init FastClick
+ */
+window.addEventListener('load', function() {
+  FastClick.attach(document.body);
+}, false);
